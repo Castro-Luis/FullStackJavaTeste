bda7edc8b64286cacd1bc743cbabf1ececa4482e
Luis Castro <luisr.contato@gmail.com>
2025-09-04 11:58:42 -0300

    Add files via upload

---
+++ Atividade 02 - Construindo objetos/src/Pacote/Pessoa.java
@@ -0,0 +1,28 @@
+package Pacote;
+
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade() {
+        this.idade = idade;
+    }
+}
