28e603a828a9398f9492fc06a8d608fa511df057
Luis Castro <luisr.contato@gmail.com>
2025-09-09 11:58:39 -0300

    Add files via upload

---
+++ Desafio 02 - Construindo objetos/src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hello, world...");
+
+        System.out.println();
+
+        var produto1 = new Produto("Óleo de cozinha", 8.29, 1);
+    }
+}
