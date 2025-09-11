c2dc1e0eeae4c73345ece43fff5e8151f7bda399
Luis Castro <luisr.contato@gmail.com>
2025-09-11 12:03:01 -0300

    Add files via upload

---
+++ Desafio 02.1 - Construindo objetos/src/PacoteProdutos/ProdutoPerecivel.java
@@ -0,0 +1,22 @@
+package PacoteProdutos;
+
+public class ProdutoPerecivel extends Produto {
+    private String dataValidade;
+
+    public ProdutoPerecivel(String nome, double preco, int quantidade) {
+        super(nome, preco, quantidade);
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto perecível: " + this.getDataValidade();
+    }
+}
---
+++ Desafio 02.1 - Construindo objetos/src/Principal.java
@@ -0,0 +1,38 @@
+//Desafio 1
+
+import PacoteProdutos.Produto;
+import PacoteProdutos.ProdutoPerecivel;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hello, world...");
+
+        //Desafio 3
+
+        var produto1 = new Produto("Óleo de cozinha", 8.29, 5);
+
+        var produto2 = new Produto("Jarra de vidro", 12.99, 1);
+
+        var produto3 = new Produto("Donuts", 5.99, 12);
+
+        var produto4 = new ProdutoPerecivel("Arroz Tio João", 7.49, 3);
+
+        //Desafio 1 - Continuação
+
+        System.out.println();
+
+        ArrayList<Produto> listaDeProdutos = new ArrayList<>();
+        listaDeProdutos.add(produto1);
+        listaDeProdutos.add(produto2);
+        listaDeProdutos.add(produto3);
+        listaDeProdutos.add(produto4);
+        System.out.println("Tamanho da lista: " + listaDeProdutos.size());
+        System.out.println("Produto selecionado: " + listaDeProdutos.get(2).getNome());
+
+        //Desafio 2
+
+        System.out.println(listaDeProdutos);
+    }
+}
