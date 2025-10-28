97cdb56930226aea8be18c46d219078b1b517cc5
Luis Castro <luisr.contato@gmail.com>
2025-10-28 11:31:50 -0300

    Add files via upload

---
+++ TesteEclipse/src/Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Eu estou me adaptando com o Eclipse.");
+		System.out.println("Ababa");
+	}
+}
+
+//Até então, utilizei por bastante tempo o Intellij. Agora, estou fazendo uma transição de IDE.
